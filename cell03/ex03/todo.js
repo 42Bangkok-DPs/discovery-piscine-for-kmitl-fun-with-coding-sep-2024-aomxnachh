875aa4385c4fe1c28b60b2c9abb5ca816bd20d30
_axmmmaccc <specialmmr@gmail.com>
2024-09-12 17:19:37 +0700

    complete

--- cell03/ex03/todo.js
+++ cell03/ex03/todo.js
@@ -1,36 +1,42 @@
-const newBtn = document.getElementById("newBtn");
-const ft_list = document.getElementById("ft_list");
+let TodoList = [];
 
-let todoCookies = document.cookie;
-if (todoCookies.length !== 0) {
-	todoCookies = todoCookies.split(";");
-	todoCookies.forEach((text) => {
-		text = text.split("=");
-		insert_new_list(text[0], text[1]);
-	});
+
+function createTodo(text) {
+    return $('<div class="todo"></div>').html('<p>' + text + '</p>' + '<button onclick="remove(\'' + text + '\')">x</button>');
 }
 
-newBtn.onclick = () => {
-	let todo_text = prompt("Add to todo");
-	if (todo_text.trim() === "") {
-		return;
-	}
 
-	let id = new Date().getTime();
-	document.cookie = `${id}=${todo_text};`;
-	insert_new_list(id, todo_text);
-};
-
-function insert_new_list(id, text) {
-	let todo = document.createElement("p");
-	todo.innerHTML = text;
-	todo.onclick = () => {
-		let isConfirm = confirm("Do you want to delete me?");
-		if (isConfirm) {
-			todo.remove();
-			document.cookie = `${id}=; Expires=Wed, 31 Dec 1969 23:59:59 GMT;`;
-		}
-	};
-
-	ft_list.prepend(todo);
+function render() {
+    $('#ft_list').empty();
+    $.each(TodoList, function(index, element) {
+        $('#ft_list').append(createTodo(element));
+    });
+    
+    localStorage.setItem('todoList', JSON.stringify(TodoList));
 }
+
+
+function newTodo() {
+    let name = prompt("Name the todo.");
+    if (name && name.length > 0) {
+        TodoList.unshift(name);
+        render();
+    }
+}
+
+
+function remove(text) {
+    let yes = confirm('Are you sure you want to remove "' + text + '"?');
+    if (yes) {
+        TodoList = TodoList.filter(todo => todo !== text);
+        render();
+    }
+}
+
+$(document).ready(function() {
+    let savedList = localStorage.getItem('todoList');
+    if (savedList) {
+        TodoList = JSON.parse(savedList);
+        render();
+    }
+});
